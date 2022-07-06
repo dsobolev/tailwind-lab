8b51ba5b2a1fc9e2c466b63f343a437a3690949b
dsobolev <denis.sobolev1@gmail.com>
2022-07-06 17:09:59 +0300

    All files commited first time

---
+++ data.js
@@ -0,0 +1,44 @@
+window.destinations = [
+    {
+        city: "Toronto",
+        avaragePrice: 120,
+        propertyCount: 76,
+        imageUrl: "img/toronto.jpg",
+        imageAlt: "Toronto skyline"
+    },
+    {
+        city: "Kharkiv",
+        avaragePrice: 75,
+        propertyCount: 31,
+        imageUrl: "img/kharkiv.jpg",
+        imageAlt: "Kharkiv city"
+    },
+    {
+        city: "Spencers W",
+        avaragePrice: 125,
+        propertyCount: 72,
+        imageUrl: "img/spensers.jpg",
+        imageAlt: "Spensers Wood views"
+    },
+    {
+        city: "London",
+        avaragePrice: 130,
+        propertyCount: 26,
+        imageUrl: "img/london.jpg",
+        imageAlt: "London House"
+    },
+    {
+        city: "Shinfield",
+        avaragePrice: 112,
+        propertyCount: 77,
+        imageUrl: "img/shinfield.jpg",
+        imageAlt: "Shinfield mead"
+    },
+    {
+        city: "Bracknel",
+        avaragePrice: 111,
+        propertyCount: 55,
+        imageUrl: "img/bracknel.jpg",
+        imageAlt: "Bracknel posh"
+    },
+];
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    content: ["index.html"],
+    darkMode: false,
+    theme: {
+        extend: {},
+    },
+    variants: {
+      extends: {
+          backgroundColor: ["active"],
+          fontSize: ["hover"]
+      }
+    },
+    plugins: [],
+}
